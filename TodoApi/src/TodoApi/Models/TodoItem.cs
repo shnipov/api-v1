eff95937e83b303208e5ce057efd2f1936859b88
Dmitry Shnipov <shnipov@gmail.com>
2016-06-14 15:04:40 +0300

    Добавил тестовый проект web api

---
+++ TodoApi/src/TodoApi/Models/TodoItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoApi.Models
+{
+    public class TodoItem
+    {
+        public string Key { get; set; }
+        public string Name { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
